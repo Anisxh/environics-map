65d9c06ad86c9249fe02dc70dea9cd5ba52222cf
Anisxh <111453465+Anisxh@users.noreply.github.com>
2022-08-20 15:43:31 +0100

    Added Wards
    Added the ward boundaries with required symbology and labelling

--- src/GISMap.tsx
+++ src/GISMap.tsx
@@ -3,6 +3,10 @@ import './css/GISMap.css';
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import { renderIntoDocument } from 'react-dom/test-utils';
+import SimpleRenderer from '@arcgis/core/renderers/SimpleRenderer';
+import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol";
+import LabelClass from "@arcgis/core/layers/support/LabelClass";
 
 function GISMap() {
   const viewDiv = useRef();
@@ -22,10 +26,36 @@ function GISMap() {
       spatialReference: {
         wkid: 102100
       },
-      center: [-79.64, 43.58],
+      center: [-79.64, 43.61],
       zoom: 10
     });
 
+    const wardLayer = new FeatureLayer({
+      url: "https://services6.arcgis.com/hM5ymMLbxIyWTjn2/arcgis/rest/services/Ward_Boundaries/FeatureServer/2",
+      renderer: new SimpleRenderer({
+        symbol: new SimpleFillSymbol({
+          color: [0, 0, 0, 0],
+          outline: {
+            width: 1,
+            color: "red"
+          }
+        })
+      }),
+      labelingInfo: [new LabelClass({
+        symbol: {
+          type: "text",
+          color: "black"
+        },
+        labelExpressionInfo: {
+          expression: "$feature.COUNCILLOR"
+        }
+      })]
+    });
+    map.add(wardLayer);
+
+    view.when(() => {
+      view.extent = wardLayer.fullExtent;
+    })
   }, []);
   return (<div id="mapDiv" ref={viewDiv}></div>);
 }
