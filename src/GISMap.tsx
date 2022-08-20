5501db0bd1cad0d03603dbac1e8806ed290f98a2
Anisxh <111453465+Anisxh@users.noreply.github.com>
2022-08-20 15:01:50 +0100

    Added Baasemap
    Added the Light Gray vector tile basemap

--- src/GISMap.tsx
+++ src/GISMap.tsx
@@ -2,26 +2,31 @@ import React, { useEffect, useRef } from 'react';
 import './css/GISMap.css';
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 function GISMap() {
   const viewDiv = useRef();
   useEffect(() => {
 
     const map = new Map({
-      basemap: "streets-vector"
+      basemap: {
+        portalItem: {
+          id: "3582b744bba84668b52a16b0b6942544" //Light gray vector tile
+        }
+      }
     });
-    
+
     const view = new MapView({
       map: map,
       container: viewDiv.current,
-      center: [-118.244, 34.052],
-      zoom: 12
+      spatialReference: {
+        wkid: 102100
+      },
+      center: [-79.64, 43.58],
+      zoom: 10
     });
-    
-    view.when(() => {
-      console.log("Map is loaded");
-    })
+
   }, []);
-  return (<div ref={viewDiv}>hello</div>);
+  return (<div id="mapDiv" ref={viewDiv}></div>);
 }
 export default GISMap;
